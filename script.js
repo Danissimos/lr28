c95523652a286ee65f18b5b021051d385c9d1dbf
Danissimos <skakovdh@mail.ru>
2019-12-19 21:44:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function s1(){
+var a = ['Снег','Ветер','Дождь','Град','Кот'];
+var myVar1 = a.join(''); 
+alert(myVar1);}
+function s2() {
+var a = ['Снег','Ветер','Дождь','Град','Кот'];   
+var myVar2 = a.join(', ');
+alert(myVar2);}
+function s3(){
+var a = ['Снег','Ветер','Дождь','Град','Кот'];
+var myVar3 = a.join(' + ');
+alert(myVar3);}
+function s4(){
+var myArray = ['один', 'два', 'три'];
+myArray.reverse();
+alert(myArray);
+}
+function s5(){
+var myArray = ['7', '5', '3'];
+myArray.sort();
+alert(myArray);
+}
